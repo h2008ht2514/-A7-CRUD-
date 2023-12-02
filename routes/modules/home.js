98a37f35fa7d953f6eb320d90756b0d57ee4e930
h2008ht2514 <h2008ht2514@example.com>
2023-12-02 15:32:29 +0800

    feat: add sort function a to z

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,47 +12,125 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
-//search function
-// 對 name 屬性按字母順序進行排序
-// Search restaurants
-router.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim().toLowerCase();
+// 在路由 /search 中處理使用者的選擇
+// 在路由 /search 中处理用户的选择
 
-  if (!keyword) {
+// 在路由 /search 中處理使用者的選擇
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword ? req.query.keyword.trim().toLowerCase() : '';
+  const sortType = req.query.sortType;
+
+  if (!keyword && !sortType) {
     return res.redirect("/");
   }
 
-  // 假設你有一個變數 sortType 用來標記排序方式
-  const sortType = req.query.sortType;
-  console.log("req.query", req.query)
+  // 如果選擇 "A > Z" 或 "Z > A"
+  if (sortType === 'az' || sortType === 'za') {
+    const sortOrder = sortType === 'az' ? 1 : -1;
 
-  if (sortType === 'AtoZ') {
-    // 如果按 A 到 Z 排序被選擇
-    res.render('index', { restaurants: sortedRestaurants, sortByAZ: true, keyword });
-  } else {
-    // 如果按 A 到 Z 排序未被選擇
-    res.render('index', { restaurants: restaurants, sortByAZ: false, keyword });
-  }
-
-  // 對 name 屬性按字母順序進行排序
-  Restaurant.find({
-    $or: [
-      { name: { $regex: keyword, $options: 'i' } },
-      { category: { $regex: keyword, $options: 'i' } }
-    ]
-  })
-    .collation({ locale: 'en', strength: 2 }) // 設定排序的collation，strength: 2表示不區分大小寫
-    .sort({ name: 1 }) // 1表示升序，-1表示降序
-    .lean()
-    .then(filteredRests => {
-      if (filteredRests.length !== 0) {
-        res.render('index', { restaurants: filteredRests, keyword });
-      } else {
-        res.render('no-results', { keyword });
-      }
+    Restaurant.find({
+      $or: [
+        { name: { $regex: keyword, $options: 'i' } },
+        { category: { $regex: keyword, $options: 'i' } }
+      ]
     })
-    .catch(err => console.log(err));
+      .collation({ locale: 'en', strength: 2 })
+      .sort({ name: sortOrder })
+      .lean()
+      .then(sortedRestaurants => {
+        console.log(sortedRestaurants);
+        res.render('index', { restaurants: sortedRestaurants, sortByAZ: sortType === 'az', keyword });
+      })
+      .catch(err => console.log(err));
+  } else {
+    // 如果選擇其他排序方式，執行原有的搜索邏輯
+    Restaurant.find({
+      $or: [
+        { name: { $regex: keyword, $options: 'i' } },
+        { category: { $regex: keyword, $options: 'i' } }
+      ]
+    })
+      .collation({ locale: 'en', strength: 2 })
+      .sort({ name: 1 })
+      .lean()
+      .then(filteredRests => {
+        console.log(filteredRests);
+        if (filteredRests.length !== 0) {
+          res.render('index', { restaurants: filteredRests, keyword });
+        } else {
+          res.render('no-results', { keyword });
+        }
+      })
+      .catch(err => console.log(err));
+  }
 });
+
+
+// router.get('/search', (req, res) => {
+//   const keyword = req.query.keyword ? req.query.keyword.trim().toLowerCase() : '';
+//   const sortType = req.query.sortType;
+
+//   if (!keyword && !sortType) {
+//     return res.redirect("/");
+//   }
+
+//   // 如果选择 "A > Z" 或 "Z > A"
+//   if (sortType === 'az' || sortType === 'za') {
+//     const sortOrder = sortType === 'az' ? 1 : -1;
+
+//     const query = {
+//       $or: [
+//         { name: { $regex: keyword, $options: 'i' } },
+//         { category: { $regex: keyword, $options: 'i' } }
+//       ]
+//     };
+
+//     // 添加额外的条件，确保只有英文餐廳受到排序影响
+//     if (sortType === 'za') {
+//       // 如果是 "Z > A" 且是英文餐廳，将排序条件改为反向
+//       query.$or.push({
+//         name: { $regex: keyword, $options: 'i', $not: /[^\x00-\x7F]/ },
+//         $where: `this.name.match(/^\\w+$/) !== null`
+//       });
+//     } else {
+//       // 如果是 "A > Z"，添加额外的条件，确保只有英文餐廳受到排序影响
+//       query.$or.push({ name: { $regex: keyword, $options: 'i', $not: /[^\x00-\x7F]/ } });
+//     }
+
+//     Restaurant.find(query)
+//       .collation({ locale: 'en', strength: 2 })
+//       .sort({ name: sortOrder })
+//       .lean()
+//       .then(sortedRestaurants => {
+//         console.log(sortedRestaurants);
+//         res.render('index', { restaurants: sortedRestaurants, sortByAZ: sortType === 'az', keyword });
+//       })
+//       .catch(err => console.log(err));
+//   } else {
+//     // 如果选择其他排序方式，执行原有的搜索逻辑
+//     Restaurant.find({
+//       $or: [
+//         { name: { $regex: keyword, $options: 'i' } },
+//         { category: { $regex: keyword, $options: 'i' } }
+//       ]
+//     })
+//       .collation({ locale: 'en', strength: 2 })
+//       .sort({ name: 1 })
+//       .lean()
+//       .then(filteredRests => {
+//         console.log(filteredRests);
+//         if (filteredRests.length !== 0) {
+//           res.render('index', { restaurants: filteredRests, keyword });
+//         } else {
+//           res.render('no-results', { keyword });
+//         }
+//       })
+//       .catch(err => console.log(err));
+//   }
+// });
+
+
+
 
 module.exports = router
 
